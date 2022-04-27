f1311a99b89d16bce3a5e5efd293471a1eeb67d2
mzpsh <mirzamiftahulfadillah@gmail.com>
2022-04-28 04:26:43 +0800

    added getBodyWeightToAgeIndex

--- lib/standar_antropometri_anak.dart
+++ lib/standar_antropometri_anak.dart
@@ -1,7 +1,42 @@
 library standar_antropometri_anak;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:standar_antropometri_anak/bodyweight_age_table.dart';
+
+class StandarAntropometriAnak {
+  static double getBodyWeightToAgeIndex(
+    num bodyWeight,
+    int ageInMonth, {
+    bool isFemale = false,
+  }) {
+    late double index;
+    late List ageRow;
+
+    if (!isFemale) {
+      ageRow = maleBodyWeightAgeTable[ageInMonth] as List;
+    } else {
+      ageRow = femaleBodyWeightAgeTable[ageInMonth] as List;
+    }
+
+    final median = ageRow[3];
+
+    if (bodyWeight == median) {
+      index = 0;
+    } else if (bodyWeight > median) {
+      final highLimit = ageRow[6];
+      final oneSD = (highLimit - median) / 3;
+
+      final difference = bodyWeight - median;
+
+      index = difference / oneSD;
+    } else {
+      final lowLimit = ageRow[0];
+      final oneSD = (lowLimit - median) / 3;
+
+      final difference = median - bodyWeight;
+
+      index = difference / oneSD;
+    }
+
+    return double.parse(index.toStringAsFixed(1));
+  }
 }
--- test/standar_antropometri_anak_test.dart
+++ test/standar_antropometri_anak_test.dart
@@ -3,10 +3,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:standar_antropometri_anak/standar_antropometri_anak.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('getBodyWeightToAgeIndex', () {
+    test(
+        'should return a correct male index if body weight is more than median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(5.0, 0);
+      expect(result, 3.0);
+    });
+    test(
+        'should return a correct male index if body weight is less than median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(2.1, 0);
+      expect(result, -3.0);
+    });
+    test('should return a correct male index if body weight is equal to median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(3.3, 0);
+      expect(result, 0);
+    });
+    test(
+        'should return a correct female index if body weight is more than median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(4.8, 0,
+          isFemale: true);
+      expect(result, 3.0);
+    });
+    test(
+        'should return a correct female index if body weight is less than median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(2.0, 0,
+          isFemale: true);
+      expect(result, -3.0);
+    });
+    test(
+        'should return a correct female index if body weight is equal to median',
+        () {
+      final result = StandarAntropometriAnak.getBodyWeightToAgeIndex(3.2, 0,
+          isFemale: true);
+      expect(result, 0);
+    });
   });
 }
